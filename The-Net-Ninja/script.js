fba95f4169eddea37b1844e3c5f937255b3356db
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2024-06-19 17:48:26 +0800

    complete: Candlestick Chart

--- The-Net-Ninja/script.js
+++ The-Net-Ninja/script.js
@@ -1,80 +1,78 @@
-const svg = d3
-  .select(".canvas")
-  .append("svg")
-  .attr("width", 600)
-  .attr("height", 600);
+const svg = d3.select('.canvas').append('svg').attr('width', 600).attr('height', 600)
 
 // create margins and dimensions
 const margin = {
-  top: 20,
-  right: 20,
-  bottom: 100,
-  left: 100,
-};
+    top: 20,
+    right: 20,
+    bottom: 100,
+    left: 100,
+}
 
-const graphWidth = 600 - margin.left - margin.right;
-const graphHeight = 600 - margin.top - margin.bottom;
+const graphWidth = 600 - margin.left - margin.right
+const graphHeight = 600 - margin.top - margin.bottom
 
 const graph = svg
-  .append("g")
-  .attr("width", graphWidth)
-  .attr("height", graphHeight)
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+    .append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`)
 
-const xAxisGroup = graph
-  .append("g")
-  .attr("transform", `translate(0, ${graphHeight})`);
-const yAxisGroup = graph.append("g");
+const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = graph.append('g')
 
-d3.json("menu.json").then((data) => {
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.orders)])
-    .range([graphHeight, 0]);
+db.collection('dishes')
+    .get()
+    .then((res) => {
+        // d3.json("menu.json").then((data) => {
+        //   const y = d3
+        //     .scaleLinear()
+        //     .domain([0, d3.max(data, (d) => d.orders)])
+        //     .range([graphHeight, 0]);
+        const data = []
 
-  const x = d3
-    .scaleBand()
-    .domain(data.map((item) => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+        res.docs.forEach((doc) => {
+            data.push(doc.data())
+        })
 
-  const min = d3.min(data, (d) => d.orders);
-  const max = d3.max(data, (d) => d.orders);
-  const extent = d3.extent(data, (d) => d.orders);
+        const x = d3
+            .scaleBand()
+            .domain(data.map((item) => item.name))
+            .range([0, 500])
+            .paddingInner(0.2)
+            .paddingOuter(0.2)
 
-  // join the data to rects
-  const rects = graph.selectAll("rect").data(data);
+        const min = d3.min(data, (d) => d.orders)
+        const max = d3.max(data, (d) => d.orders)
+        const extent = d3.extent(data, (d) => d.orders)
 
-  rects
-    .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
-    .attr("fill", "orange")
-    .attr("x", (d) => x(d.name));
+        // join the data to rects
+        const rects = graph.selectAll('rect').data(data)
 
-  // append the enter selection to the DOM
-  rects
-    .enter()
-    .append("rect")
-    .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
-    .attr("fill", "orange")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders));
+        rects
+            .attr('width', x.bandwidth)
+            .attr('height', (d) => graphHeight - y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', (d) => x(d.name))
 
-  // create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3
-    .axisLeft(y)
-    .ticks(3)
-    .tickFormat((d) => d + " orders");
+        // append the enter selection to the DOM
+        rects
+            .enter()
+            .append('rect')
+            .attr('width', x.bandwidth)
+            .attr('height', (d) => graphHeight - y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', (d) => x(d.name))
+            .attr('y', (d) => y(d.orders))
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+        // create and call the axes
+        const xAxis = d3.axisBottom(x)
+        const yAxis = d3
+            .axisLeft(y)
+            .ticks(3)
+            .tickFormat((d) => d + ' orders')
 
-  xAxisGroup
-    .selectAll("text")
-    .attr("transform", "rotate(-40)")
-    .attr("text-anchor", "end")
-    .attr("fill", "orange");
-});
+        xAxisGroup.call(xAxis)
+        yAxisGroup.call(yAxis)
+
+        xAxisGroup.selectAll('text').attr('transform', 'rotate(-40)').attr('text-anchor', 'end').attr('fill', 'orange')
+    })
